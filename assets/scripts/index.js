480f9dd17d2fbec71b5a68b8feec39f83423a94c
Jeremy Henry <jeremyh416@gmail.com>
2021-08-28 14:16:27 -0500

    initial pseudocode added

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+// build HTML layout
+// input text field to get what user wants to search for
+// Need to know what URL looks like for Library of Congress API
+// Find API documentation for LoC API
+// add searchResults.html file to project
+// may need to splice together URL based on user's search text
+// determine categories for user to select from - include in drop-down
+// need event listener on button to perform search.
+// 
